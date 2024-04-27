0b5bfaeddc330334df3750c5a36e36d989ca0aab
avila <avila.dev@outlook.com>
2024-04-27 00:22:22 -0300

    feat [wip]: add saveProduct implementation

--- src/main/java/com/avila/commerce/service/ProductService.java
+++ src/main/java/com/avila/commerce/service/ProductService.java
@@ -1,9 +1,55 @@
 package com.avila.commerce.service;
+import com.avila.commerce.contract.ProductContract;
+import com.avila.commerce.model.Product;
+import com.avila.commerce.repository.ProductRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+import java.util.List;
+import java.util.Optional;
 
+@Service @AllArgsConstructor
+public class ProductService implements ProductContract {
+    private final ProductRepository productRepository;
 
-public class ProductService {
+    @Override
+    public Product saveProduct(Product product) {
+        return productRepository.save(product);
+    }
 
+    // TODO ------------------------------------------------------>
 
+    @Override
+    public Product getProduct(Product product) {
+        return null;
+    }
 
+    @Override
+    public Product updateProduct(Product product) {
+        return null;
+    }
 
+    @Override
+    public Product deleteProduct(Product product) {
+        return null;
+    }
+
+    @Override
+    public List<Product> getAllProducts() {
+        return List.of();
+    }
+
+    @Override
+    public List<Product> getProductsByCategory(String category) {
+        return List.of();
+    }
+
+    @Override
+    public Optional<Product> getProductById(Long id) {
+        return Optional.empty();
+    }
+
+    @Override
+    public Optional<Product> getProductByName(String name) {
+        return Optional.empty();
+    }
 }
