a24b9d59d4c4e83a8210a8b04f0b64f565df6bc8
renato avila <avila>
2024-04-28 15:27:54 -0300

    [ignore] test purpose

--- src/main/java/com/avila/commerce/CommerceApplication.java
+++ src/main/java/com/avila/commerce/CommerceApplication.java
@@ -1,5 +1,4 @@
 package com.avila.commerce;
-import com.avila.commerce.model.Product;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,6 +8,4 @@ public class CommerceApplication {
     public static void main(String[] args) {
         SpringApplication.run(CommerceApplication.class, args);
     }
-
-
 }
