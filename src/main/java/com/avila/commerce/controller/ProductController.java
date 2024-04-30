36c1b3cd9c32260f4c9ebc625128b28e5f022532
avila-r <avila.dev@outlook.com>
2024-04-30 13:21:05 -0300

    feat: implement CRUD controllers

--- src/main/java/com/avila/commerce/controller/ProductController.java
+++ src/main/java/com/avila/commerce/controller/ProductController.java
@@ -4,9 +4,8 @@ import com.avila.commerce.service.ProductAccessService;
 import com.avila.commerce.service.ProductService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
 
 @RestController @RequestMapping("/commerce/product")
 @AllArgsConstructor
@@ -15,11 +14,66 @@ public class ProductController {
     private final ProductService productService;
 
     @PostMapping
-    public ResponseEntity<Product.ProductResponseDTO> saveProduct(Product.ProductRequestDTO productRequestDTO) {
+    public ResponseEntity<Product.ProductResponseDTO> saveProduct(@RequestBody Product.ProductRequestDTO productRequestDTO) {
         return ResponseEntity.ok(
                 productAccessService.convertProductToResponse(
                         productService.saveProduct(productAccessService.convertRequestToProduct(productRequestDTO))
                 )
+        );
+    }
+
+    @PostMapping("/delete")
+    public ResponseEntity<String> deleteProduct(@RequestBody Product.ProductRequestDTO productRequestDTO){
+        return productService.deleteProduct(productAccessService.convertRequestToProduct(productRequestDTO));
+    }
+
+    @PostMapping("/update")
+    public ResponseEntity<Product.ProductResponseDTO> updateProduct(@RequestBody Product.ProductRequestDTO productRequestDTO){
+        return ResponseEntity.ok(
+                productAccessService.convertProductToResponse(
+                        productService.updateProduct(productAccessService.convertRequestToProduct(productRequestDTO))
+                )
+        );
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Product.ProductResponseDTO>> getAll(){
+        return ResponseEntity.ok(
+                productService.getAllProducts().stream()
+                        .map(productAccessService::convertProductToResponse)
+                        .toList()
+        );
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Product.ProductResponseDTO> getByIdEndpoint(@PathVariable Long id){
+        return ResponseEntity.ok(
+                productAccessService.convertProductToResponse(productService.getProductById(id))
+        );
+    }
+
+    @GetMapping("/id")
+    public ResponseEntity<Product.ProductResponseDTO> getByIdParam(@RequestParam(name = "id", defaultValue = "0") Long id){
+        return ResponseEntity.ok(
+                productAccessService.convertProductToResponse(productService.getProductById(id))
+        );
+    }
+
+    @GetMapping("/category/{category}")
+    public ResponseEntity<List<Product.ProductResponseDTO>> listByCategoryEndpoint(@PathVariable String category){
+        return ResponseEntity.ok(
+                productService.getProductsByCategory(category).stream()
+                        .map(productAccessService::convertProductToResponse)
+                        .toList()
+        );
+    }
+
+    @GetMapping("/category")
+    public ResponseEntity<List<Product.ProductResponseDTO>> listByCategoryParam(@RequestParam(name = "category") String category){
+        return ResponseEntity.ok(
+                productService.getProductsByCategory(category).stream()
+                        .map(productAccessService::convertProductToResponse)
+                        .toList()
         );
     }
 }
