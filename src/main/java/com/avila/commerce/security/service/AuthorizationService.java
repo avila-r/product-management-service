6a2a90daa1e0c992d21ba86316a3cab93ee1d7a4
avila-r <avila.dev@outlook.com>
2024-04-30 18:17:03 -0300

    feat: define service to publish user details bean

---
+++ src/main/java/com/avila/commerce/security/service/AuthorizationService.java
@@ -0,0 +1,17 @@
+package com.avila.commerce.security.service;
+import com.avila.commerce.security.repository.UserRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service @AllArgsConstructor
+public class AuthorizationService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByLogin(username);
+    }
+}
