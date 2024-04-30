5a0f888cc37d6c02b38e9793b094aaac2bd24c7b
avila-r <avila.dev@outlook.com>
2024-04-30 18:12:26 -0300

    fix: add repository annotation

--- src/main/java/com/avila/commerce/repository/ProductRepository.java
+++ src/main/java/com/avila/commerce/repository/ProductRepository.java
@@ -1,9 +1,11 @@
 package com.avila.commerce.repository;
 import com.avila.commerce.model.Product;
 import org.springframework.data.repository.ListCrudRepository;
+import org.springframework.stereotype.Repository;
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface ProductRepository extends ListCrudRepository<Product, Long> {
     Optional<Product> findByName(String name);
     boolean existsByName(String name);
