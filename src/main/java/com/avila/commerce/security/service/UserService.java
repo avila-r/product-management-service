2a700ebafbcd03080e40275eb2fd31a88b3a5d87
avila-r <avila.dev@outlook.com>
2024-04-30 18:16:01 -0300

    feat: define user creation method

---
+++ src/main/java/com/avila/commerce/security/service/UserService.java
@@ -0,0 +1,23 @@
+package com.avila.commerce.security.service;
+import com.avila.commerce.exception.user.UsernameAlreadyExistsException;
+import com.avila.commerce.security.model.Registration;
+import com.avila.commerce.security.model.User;
+import com.avila.commerce.security.repository.UserRepository;
+import lombok.AllArgsConstructor;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service @AllArgsConstructor
+public class UserService {
+    private final UserRepository userRepository;
+
+    public User createUser(@NotNull Registration registration){
+        if (userRepository.existsByLogin(registration.login())) throw new UsernameAlreadyExistsException("Username already exists");
+        else return userRepository.save(new User(
+                registration.role(),
+                registration.login(),
+                new BCryptPasswordEncoder().encode(registration.password())
+        ));
+    }
+}
